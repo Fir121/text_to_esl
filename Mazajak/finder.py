4b1e0708e49e5addde5f87eb58a964779545c49f
Mohamed Firas <62911996+Fir121@users.noreply.github.com>
2023-10-04 16:31:29 +0400

    mazajak

--- Mazajak/finder.py
+++ Mazajak/finder.py
@@ -1,7 +1,31 @@
 import gensim
+import pandas as pd
+from operator import itemgetter
 
-embeddings = gensim.models.KeyedVectors.load_word2vec_format('data/w2vec_sg.bin',binary=True,unicode_errors='ignore')
+excel_file = "data/output-excel.csv"
+df = pd.read_csv(excel_file)
 
-sims = embeddings.wv.most_similar('مركبة', topn=10) 
+multi_words = []
+solo_words = []
+letters = []
 
-print(sims)
+for index, row in df.iterrows():
+    word = str(row[0])
+    if len(word.split(" ")) > 1:
+        multi_words.append(word)
+    elif len(word) == 1:
+        letters.append(word)
+    else:
+        solo_words.append(word)
+
+embeddings = gensim.models.KeyedVectors.load_word2vec_format('Mazajak/data/sg_250.bin',binary=True,unicode_errors='ignore')
+
+d_arr = {}
+inp = 'سيارات'
+for word in solo_words:
+    try:
+        d_arr[word] = embeddings.similarity(inp, word)
+    except:
+        pass
+print(sorted(d_arr.items(), key=itemgetter(1), reverse=True)[:5])
+print(sorted(d_arr, key=d_arr.get, reverse=True)[:5])
